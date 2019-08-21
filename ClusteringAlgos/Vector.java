2f99dc5f8ceefb82c8b6b7f7c3373c1e707c51ae
Santosh Sivakumar <51939734+santosh-sivakumar@users.noreply.github.com>
2019-08-20 17:09:14 -0700

    Syntax update for final commit - 8/20/19

--- ClusteringAlgos/Vector.java
+++ ClusteringAlgos/Vector.java
@@ -1,19 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
 import java.lang.Math;
 
+// public vector class - represents a given object within a cluster
 public class Vector {
 	
-	List<Double> numbers = new ArrayList<Double>();
+	ArrayList<Double> numbers = new ArrayList<Double>();
 	String name;
 	
-	Vector (String inputName, List<Double> inputNumbers) {
-	// public vector class - represents a given object within a cluster
+	Vector (String inputName, ArrayList<Double> inputNumbers) {
 		name = inputName;
 		numbers = inputNumbers;
 	}
-		
-	public double magnitude() { 
+
 	// returns vector magnitude, calculated to be the square root of sum(coordinates^2)
+	public double magnitude() { 
 		assert (numbers.size() > 0) : "Incompatible # dimensions";
 		double magnitude = 0.0;
 
@@ -23,23 +23,20 @@ public class Vector {
 			
 		return Math.sqrt(magnitude);
 	}
-
-	public static double distance(Vector vector1, Vector vector2) throws RuntimeException { 
-	// returns distance between two Vectors of equal num dimensions
+	
+	// returns Euclidian distance between two Vectors of equal num dimensions
+	public double distance(Vector vector1, Vector vector2){ 
 		double squaredDistance = 0.0;
 
-		if (vector1.numbers.size() != vector2.numbers.size()) {
-			throw new RuntimeException();
-		}
-			
+		assert (vector1.numbers.size() == vector2.numbers.size()): "Vectors not compatible";
+
 		for (int index = 0; index < vector1.numbers.size(); index++) {
 			double difference = vector1.numbers.get(index) - vector2.numbers.get(index);
 			difference = difference * difference;
 			squaredDistance = squaredDistance + difference;
 		}
-			
+			 
 		double distance = Math.sqrt(squaredDistance);	
 		return distance;
 	}
 }
-
