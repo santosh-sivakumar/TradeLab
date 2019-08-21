38ccfc5e7ff8cee4bb84c458dd88164c787d6388
Santosh Sivakumar <51939734+santosh-sivakumar@users.noreply.github.com>
2019-08-20 17:50:34 -0700

    renamed, refactored for final commit

--- TradingStrategies/MeanReversionStrategy.java
+++ TradingStrategies/MeanReversionStrategy.java
@@ -1,16 +1,16 @@
-import java.util.*;
+import java.util.ArrayList;
 
-public class ThresholdStrategy {
+// public class to initialize new instance of the Threshold Strategy
+// threshold percentage = range outside of which stocks should be sold/bought
+// volatile percentage = what fraction of stock should be sold when deciding to sell
+public class MeanReversionStrategy {
 
 	String name;
 	Double thresholdPercentage;
 	Double volatilePercentage;
-	List<Double> initialStockValues;
+	ArrayList<Double> initialStockValues;
 		
-	ThresholdStrategy (String inputName, double inputThresholdPercentage, double inputVolatilePercentage) {
-	// public class to initialize new instance of the Threshold Strategy
-	// threshold percentage = range outside of which stocks should be sold/bought
-	// volatile percentage = what fraction of stock should be sold when deciding to sell
+	MeanReversionStrategy (String inputName, double inputThresholdPercentage, double inputVolatilePercentage) {
 
 		name = inputName;
 		thresholdPercentage = inputThresholdPercentage;
@@ -18,10 +18,10 @@ public class ThresholdStrategy {
 		initialStockValues = new ArrayList<Double>();
 	}
 
-	public void initialize (Portfolio port, DataCollection dc, DateModifications dm, String date) {
 	// public method to initialize instance of Threshold Strategy on given date
 	// if all values not recorded on given date, decrement date using DataCollection method
-	// for all stocks in portfolio, retrieve share price on given date + add to list
+	// for all stocks in portfolio, retrieve share price on given date + add to ArrayList
+	public void initialize (Portfolio port, DataCollection dc, DateModifications dm, String date) {
 
 		while (!dc.checkIfPortfolioDataExists(port, date)) {
 			date = dm.incrementDate(date);
@@ -38,11 +38,11 @@ public class ThresholdStrategy {
 		}
 	}
 
-	private boolean aboveThreshold (Portfolio port, int stockIndex, Value sharePrice) {
 	// method to determine if stock price on given date is above threshold
 	// retrieves center value of stock + multiplies by threshold factor
 	// if share price on given day above upper threshold, return false. Else, true
-	
+	private boolean aboveThreshold (Portfolio port, int stockIndex, Value sharePrice) {
+
 		double upperThreshold = (1.0 + thresholdPercentage);
 		double upperLimit = (initialStockValues.get(stockIndex) * (upperThreshold));
 	
@@ -51,13 +51,12 @@ public class ThresholdStrategy {
 		}
 		return false;
 	}
-	
-	private boolean belowThreshold (Portfolio port, int stockIndex, Value sharePrice) {
+
 	// method to determine if stock price on given date is below threshold
 	// retrieves center value of stock + multiplies by threshold factor (subtracting)
 	// if share price on given day below lower threshold, return false. Else, true
+	private boolean belowThreshold (Portfolio port, int stockIndex, Value sharePrice) {
 
-			
 		double lowerThreshold = (1.0 - thresholdPercentage);
 		double lowerLimit = (initialStockValues.get(stockIndex) * (lowerThreshold));
 
@@ -67,28 +66,27 @@ public class ThresholdStrategy {
 		}
 		return false;
 	}
-	
-	private void rebalancePortfolioSell (Portfolio port, int stockIndex, String stockID, DataCollection dc) {
+
 	// method sells shares of stock at given index
 	// determines current amount invested in stock, computes amount to sell using volatile %
 	// sell: add dollars made to cash holdings, subtract $$ from amount invested in stock
+	private void rebalancePortfolioSell (Portfolio port, int stockIndex, String stockID, DataCollection dc) {
 		
 		double currNumShares = (port.valuesInPortfolio).get(stockIndex);
 		double currPriceOfShare = dc.dataPoints.get(stockID).close;
-		double numSharesToSell = currNumShares * volatilePercentage;
+		double numSharesToSell = currNumShares * (0.01 * volatilePercentage);
 		double dollarsToMake = currPriceOfShare * numSharesToSell;
 		port.cash += dollarsToMake;
 		double updatedNumShares = (port.valuesInPortfolio).get(stockIndex) - numSharesToSell;
 		(port.valuesInPortfolio).set(stockIndex, updatedNumShares);
 	}
-	
-	private void rebalancePortfolioBuy (double numStocksToBuy, Portfolio port, int stockIndex, 
-		String stockID, DataCollection dc) {
+
 	// method sells shares of stock at given index
 	// sum = total amount invested among all stocks which will be bought in this trading day
 	// computes percentage of sum account for by given stock (stock Proportion)
 	// if amount of loose cash exceeds cash threshold, execute "buy operation"
 	// buy: subtract dollars spent from cash holdings, add $$ to amount invested in stock
+	private void rebalancePortfolioBuy (double numStocksToBuy, Portfolio port, int stockIndex, String stockID, DataCollection dc) {
 
 		double CASH_THRESHOLD = 10.0;
 		double stockFraction = (1.0 / numStocksToBuy);
@@ -104,16 +102,16 @@ public class ThresholdStrategy {
 		}
 	}
 
-	private void update (Portfolio port, String currDate, DataCollection dc) {
 	// method to execute one day of trading strategy
-	// initializes list of all stocks to be bought on given day (executed after sales)
+	// initializes ArrayList of all stocks to be bought on given day (executed after sales)
 	// iterates through stocks in portfolio, assembles ID using stock name + given date
 	// determines whether given date's share price exceeds/falls short of threshold limit
-	// if below, add stock to list of stocks to buy. If above, execute "sell" operation
+	// if below, add stock to ArrayList of stocks to buy. If above, execute "sell" operation
 	// once all sales have been made, compute sum of current $$ invested in all stocks to be bought
-	// for each stock in list to buy, execute "buy" operation
+	// for each stock in ArrayList to buy, execute "buy" operation
+	private void update (Portfolio port, String currDate, DataCollection dc) {
 	
-		List<Integer> indexOfStocksToBuy = new ArrayList<Integer>();
+		ArrayList<Integer> indexOfStocksToBuy = new ArrayList<Integer>();
 	
 		for (int index = 0; index < (port.stocksInPortfolio).size(); index++) {
 			String stockName = (port.stocksInPortfolio).get(index);
@@ -138,13 +136,12 @@ public class ThresholdStrategy {
 		}
 	}
 
-	public void allUpdates (Portfolio port, DataCollection dc, DateModifications dm,
-	 String startDate, String endDate) {
 	// method to execute trading strategy daily within a certain time period
 	// calls dataCollection increment date method until all stocks in portfolio are recorded
 	// executes trading strategy on given date (update method), updates date
 	// for each date update, method checks to ensure portfolio contains all stocks
-	
+	public void allUpdates (Portfolio port, DataCollection dc, DateModifications dm, String startDate, String endDate) {
+
 		while (!dc.checkIfPortfolioDataExists(port, startDate)) {
 			startDate = dm.incrementDate(startDate);
 		}
@@ -159,8 +156,8 @@ public class ThresholdStrategy {
 			while (!dc.checkIfPortfolioDataExists(port, currDate)) {
 				currDate = dm.incrementDate(currDate);
 			}
+			System.out.println(dc.valueOfPortfolio(port,currDate, dm));
+			System.out.println(port.cash);
 		}
 	}
 }
-			
-			
