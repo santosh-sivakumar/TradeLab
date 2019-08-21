0be727ca37cbaaaeb792578dcb43ee6f5239e373
Santosh Sivakumar <51939734+santosh-sivakumar@users.noreply.github.com>
2019-08-20 17:21:04 -0700

    refactoring for final commit

--- HelperClasses/DateModifications.java
+++ HelperClasses/DateModifications.java
@@ -1,24 +1,25 @@
-public class DateModifications {
 // public class to make modifications to a given date
-
+public class DateModifications {
+	
+	// returns year of a given date (input format = String)
 	private int dateYear (String date) {
 		int year = Integer.parseInt(date.substring(0,4));
 		return year;
-	// returns year of a given date (input format = String)
 	}
 	
+	// returns month of a given date (input format = String)
 	private int dateMonth (String date) {
 		int month = Integer.parseInt(date.substring(5,7));
 		return month;
-	// returns month of a given date (input format = String)
 	}
 	
+	// returns day of a given date (input format = String)
 	private int dateDay (String date) {
 		int day = Integer.parseInt(date.substring(8,10));
 		return day;	
-	// returns day of a given date (input format = String)
 	}
 	
+	// returns whether date is within a leap year, as a boolean value
 	private boolean isLeapYear (String date) {
 		int year = dateYear(date);
 		if ((year % 4) == 0) {
@@ -31,23 +32,23 @@ public class DateModifications {
 			return true;
 		}
 		return false;
-	// returns whether date is within a leap year, as a boolean value
 	}
-	
+
+	// for incrementing purposes, determines number of days in month
+	// returns True if month is 30 days, false elsewhere [incl. Feb]
 	private boolean isMonthThirtyDays (String date) {
 		int month = dateMonth(date);
 		if (month == 4 || month == 6 || month == 9 || month == 1) {
 			return true;
 		}
 		return false;
-	// for incrementing purposes, determines number of days in month
-	// returns True if month is 30 days, false elsewhere [incl. Feb]
+
 	}
-		
-	public String incrementDate (String date) {
+	
 	// main public method to forward increment a date (given as a String)
 	// runs through possible scenarios (different times in the year)
-	// updates date as necessary, returns incremented date as a String
+	// updates date as necessary, returns incremented date as a String	
+	public String incrementDate (String date) {
 
 		if (dateDay(date) == 28 && dateMonth(date) == 2 && (!isLeapYear(date))) {
 			String year = Integer.toString(dateYear(date));
@@ -106,11 +107,10 @@ public class DateModifications {
 		return date;
 	}
 
-	public String decrementDate (String date) {
 	// main public method to backward decrement a date (given as a String)
 	// runs through possible scenarios (different times in the year)
 	// updates date as necessary, returns decremented date as a String
-
+	public String decrementDate (String date) {
 
 		if (dateDay(date) == 3 && dateMonth(date) == 1 && (!isLeapYear(date))) {
 			String year = Integer.toString(dateYear(date));
@@ -170,4 +170,3 @@ public class DateModifications {
 	}
 
 }
-
